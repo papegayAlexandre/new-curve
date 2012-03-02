05fe939dfeb73114f38f94c161c540372e55e13f
Gregor <Gregor@delle>
2012-03-02 03:23:31 +0100

    Testing move method

--- src/models/Player.java
+++ src/models/Player.java
@@ -11,12 +11,35 @@ public class Player {
 	private Color color;
 	private int x;
 	private int y;
+	/** int between 0 and 360 (degrees) */
+	private int orientation;
 	
-	private ArrayList<Point> trail; 
-	
+	private int speed;
+
+	private ArrayList<Point> trail;
+
 	public Player(String name, Color color) {
 		setName(name);
 		setColor(color);
+	}
+
+	public void move(int delta) {
+		int xThrust = 0, yThrust = 0;
+		if (orientation <= 90) {
+			xThrust = orientation;
+			yThrust = 90 - orientation;
+		} else if (orientation <= 180) {
+			xThrust = 180 - orientation;
+			yThrust = 90 - orientation;
+		} else if (orientation <= 270) {
+			xThrust = 180 - orientation;
+			yThrust = orientation - 270;
+		} else if (orientation <= 359) {
+			xThrust = orientation - 360;
+			yThrust = orientation - 270;
+		}
+		setX(Math.round(xThrust * speed * delta / 1000f));
+		setY(Math.round(yThrust * speed * delta / 1000f));
 	}
 
 	public String getName() {
@@ -30,11 +53,10 @@ public class Player {
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
 
 	public int getX() {
 		return x;
@@ -52,5 +74,16 @@ public class Player {
 		this.y = y;
 	}
 
+	public int getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(int orientation) {
+		while (orientation < 0 || orientation > 359) {
+			orientation = orientation < 0 ? orientation + 360
+					: orientation - 360;
+		}
+		this.orientation = orientation;
+	}
 
 }
